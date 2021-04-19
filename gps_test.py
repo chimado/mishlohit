a2f2d31b577d3b2844d79cd7661f08f7f6ebbb61
chimado <ido.ludmer@gmail.com>
2021-04-19 10:23:07 +0300

    changed_print_output

--- gps_test.py
+++ gps_test.py
@@ -1,8 +1,10 @@
 from gps import *
 import time
 
+# run sudo gpsd /dev/serial0 -F /var/run/gpsd.sock here
+
 gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
-print 'latitude\tlongitude\ttime utc\t\t\taltitude\tepv\tept\tspeed\tclimb'
+# print 'latitude\tlongitude\ttime utc\t\t\taltitude\tepv\tept\tspeed\tclimb'
 
 try:
 
@@ -11,14 +13,18 @@ try:
 		report = gpsd.next()
 		if report['class'] == 'TPV':
 
-			print getattr(report,'lat',0.0),"\t",
-			print getattr(report,'lon',0.0),"\t",
-			print getattr(report,'time',''),"\t",
-			print getattr(report,'alt','nan'),"\t\t",
-			print getattr(report,'epv','nan'),"\t",
-			print getattr(report,'ept','nan'),"\t",
-			print getattr(report,'speed','nan'),"\t",
-			print getattr(report,'climb','nan'),"\t"
+			print ("latitude")
+			print getattr(report,'lat',0.0)
+			print ("longitude")
+			print getattr(report,'lon',0.0)
+#			print getattr(report,'lat',0.0),"\t",
+#			print getattr(report,'lon',0.0),"\t",
+#			print getattr(report,'time',''),"\t",
+#			print getattr(report,'alt','nan'),"\t\t",
+#			print getattr(report,'epv','nan'),"\t",
+#			print getattr(report,'ept','nan'),"\t",
+#			print getattr(report,'speed','nan'),"\t",
+#			print getattr(report,'climb','nan'),"\t"
 
 		time.sleep(1)
 
