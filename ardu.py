bfa3de42e90d9884f4aa744aed04480c1349903a
chimado <ido.ludmer@gmail.com>
2021-05-03 17:43:46 +0300

    serial_comms_working

--- ardu.py
+++ ardu.py
@@ -5,7 +5,7 @@ import csv
 ser = serial.Serial('/dev/ttyUSB0')
 ser.flushInput()
 
-for i in 20:
+for i in range(20):
     ser_bytes = ser.readline()
-    decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
+    decoded_bytes = str(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
     print(decoded_bytes)
